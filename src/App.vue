7613da02f2777f878fb037d8da4fd50fefa8fbe5
nehabht <ne.bhatti2@icloud.com>
2022-05-05 16:04:02 +0200

    initial commit and header

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '@/components/HeaderComponent.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/style.scss";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  color: #2c3e50;
+  overflow: auto;
+}
+
+img{
+  max-width: 100%;
+}
+
+</style>
+
+
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="d-flex">
+        <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png" alt="">
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header{
+    height: 50px;
+    background-color: #2E3A46;
+}
+
+img{
+    height: 40px;
+    margin: 5px 10px;
+}
+</style>
