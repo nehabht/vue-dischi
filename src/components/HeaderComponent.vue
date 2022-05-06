1ae9bc4ac9a46d482b65233003df8cdfc99ebff7
nehabht <ne.bhatti2@icloud.com>
2022-05-06 16:34:02 +0200

    add dropdown

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,17 +1,31 @@
 <template>
-    <header class="d-flex">
-        <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png" alt="">
-    </header>
+  <header class="d-flex justify-content-between">
+    <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png" alt="">
+    <div class="selecterGenere">
+      <label for="gender_album" class="text-white fs-5 fw-bold pt-2 pe-3">Genere:</label>
+      <select
+        name="gender_album"
+        id="gender_album">
+        <option value="all">All</option>
+        <option value="rock">Rock</option>
+        <option value="pop">Pop</option>
+        <option value="jazz">Jazz</option>
+        <option value="metal">Metal</option>
+      </select>
+    </div>
+    
+  </header>
 </template>
 
 <style lang="scss" scoped>
-header{
-    height: 50px;
-    background-color: #2E3A46;
+header {
+  height: 50px;
+  background-color: #2e3a46;
+  padding-right: 40px
 }
 
-img{
-    height: 40px;
-    margin: 5px 10px;
+img {
+  height: 40px;
+  margin: 5px 40px;
 }
 </style>
